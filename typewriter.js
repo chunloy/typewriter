a723ca948c7a5588a7b1e6ed26ff8ebd04b36bf8
Aaron Au <aaron.au@me.com>
2022-04-12 21:06:44 +0000

    Added comments to code.

--- typewriter.js
+++ typewriter.js
@@ -2,12 +2,10 @@ const sentence = "hello there from lighthouse labs";
 const totalTime = sentence.length * 50;
 let timeOut = 0;
 
+//queue up delays for each character 50ms apart
 for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, timeOut += 50);
+  setTimeout(() => process.stdout.write(char), timeOut += 50);
 }
 
-setTimeout(() => {
-  process.stdout.write('\n');
-}, totalTime);
+//print a new line after all characters were printed
+setTimeout(() => process.stdout.write('\n'), totalTime);
